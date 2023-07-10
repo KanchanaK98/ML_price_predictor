6be49e31b45c2a185598b47af134b79b891b4eb7
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2023-07-10 18:38:37 +0530

    initialized frontend and backend

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+import './App.css';
+
+import { Form } from './Form';
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
+  }
+
+}
+
+export default App;
