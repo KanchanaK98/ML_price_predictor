456770388444d6666179d29f38813d97efb40052
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2023-07-11 22:52:36 +0530

    modified server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,34 +3,42 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const fs = require('fs');
+const pickle = require('pickle');
 
 
-// const dbConnection = require("./src/Config/connectDataBase");
-
-// const userAuthRoutes = require("./src/routes/UserAuthRoutes/UserAuth")
-// const salaryPaymentRoutes = require("./src/routes/SalaryPaymentModule/salaryPaymentRoutes");
-// const employeeRouter = require("./src/routes/ReportersManagementModule/employeeRoutes");
-// const recruitmentRoutes = require("./src/routes/RecruitmentModule/recruitmentRoutes");
-// const assetRoutes = require("./src/routes/AssetsManagementModule/assetRoutes");
-// const leaveRoutes = require("./src/routes/LeaveModule/leaveRoutes");
-// const promotionRoutes = require("./src/routes/PromotionModule/promotionRoutes");
 
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 8070;
 
-// dbConnection();
+// Define your API endpoint(s)
+app.post('/predict', (req, res) => {
+    const datas = [8, 1.3, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0]
+    fs.readFile('predictor.pickle', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error loading model');
+            return;
+        }
+
+        const model = pickle.loads(data);
+
+        // Get the input data from the request body
+        // const inputData = req.body.data;
+        const inputData = datas
+
+        // Perform prediction using the loaded model
+        const prediction = model.predict([inputData]);
+
+        // Return the prediction result as the response
+        res.json({ prediction });
+        console.log(prediction)
+    });
+});
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} port`);
 });
-
-// app.use("/", userAuthRoutes);
-// app.use("/recruitment", recruitmentRoutes);
-// app.use("/salary", salaryPaymentRoutes);
-// app.use("/assets", assetRoutes);
-// app.use("/employee", employeeRouter);
-// app.use("/leave", leaveRoutes);
-// app.use("/promotion", promotionRoutes);
